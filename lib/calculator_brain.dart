78e022fb00c1a989248490f166431fdb064a874c
koladeore <koladeore@gmail.com>
2020-12-04 21:09:58 +0100

    first commit

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain({this.height, this.weight});
+  final int height;
+  final int weight;
+  double _bmi;
+  String calculateBMI() {
+    // weight divide by height raise to power of two and divide by 100 to convert cm to m
+    _bmi = weight / pow(height / 100, 2);
+    // one decimal place
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal body weight. Try to exercise more.';
+    } else if (_bmi > 18.5) {
+      return 'You have a normal body weight. Good job!';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more';
+    }
+  }
+}
